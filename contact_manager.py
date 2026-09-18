64ee013388bdf1cba5f608c216372a043e24647a
Leonardo Prokopowiski <leoprokopowiskii@gmail.com>
2026-09-18 10:07:54 -0300

    feat: add contact manager menu

---
+++ contact_manager.py
@@ -0,0 +1,15 @@
+while True:
+    print("\n----- Contacts manager -----")
+    print("1. Add a contact")
+    print("2. View contacts")
+    print("3. Edit a contact")
+    print("4. Mark or unmark contact as favorite")
+    print("5. View contacts marked as favorite")
+    print("6. Exit")
+
+    choice = input("\nEnter your choice: ")
+
+    if choice == "6":
+        break
+
+print("Finish program!")
