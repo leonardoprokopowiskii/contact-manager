2e5e9409083698906f8bd856b53a76a00951ae2b
Leonardo Prokopowiski <leoprokopowiskii@gmail.com>
2026-09-18 10:33:04 -0300

    feat: add function to add a contact

--- contact_manager.py
+++ contact_manager.py
@@ -1,3 +1,15 @@
+def add_contact(contacts, contact_name, contact_phone, contact_email):
+    contact = {
+        "name": contact_name,
+        "phone": contact_phone,
+        "email": contact_email,
+        "favorite": False,
+    }
+    contacts.append(contact)
+    print(f"\nContact '{contact_name}' added successfully!")
+
+contacts = []
+
 while True:
     print("\n----- Contacts manager -----")
     print("1. Add a contact")
@@ -9,7 +21,12 @@ while True:
 
     choice = input("\nEnter your choice: ")
 
-    if choice == "6":
+    if choice == "1":
+        contact_name = input("Enter the name of contact: ")
+        contact_phone = input("Enter the phone of contact: ")
+        contact_email = input("Enter the email of contact: ")
+        add_contact(contacts, contact_name, contact_phone, contact_email)
+    elif choice == "6":
         break
 
 print("Finish program!")
